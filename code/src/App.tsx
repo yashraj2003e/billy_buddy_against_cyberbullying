b081c56b32c57a61db4fa72d0b8f9fa2abeb44a5
Yashraj <yashsharmau9@gmail.com>
2024-11-18 21:33:46 +0530

    Added api key for virus total

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,8 +1,10 @@
 import { getBase64 } from "./utils/utils";
 
 export default function App() {
+  const key = import.meta.env.VITE_VIRUS_TOTAL_KEY;
   return (
     <h1>
+      {key}
       {getBase64("https://www.leetcode.com")}{" "}
       {" aHR0cHM6Ly93d3cubGVldGNvZGUuY29t"}
     </h1>
