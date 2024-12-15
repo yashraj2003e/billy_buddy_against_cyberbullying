fc4a8302ec443d1f90aae5519a710380f7437db0
Yashraj <yashsharmau9@gmail.com>
2024-12-16 05:01:50 +0530

    Added websocket connection and chat feature

---
+++ api/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => res.send("HELLO"));
+
+let users = 0;
+
+let messages = [];
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} joined the room !}`);
+  users++;
+  socket.emit("setUsers", users, messages);
+  socket.broadcast.emit("setUsers", users);
+  socket.on("disconnecting", () => {
+    users--;
+    socket.broadcast.emit("setUsers", users);
+  });
+  socket.on("updateMessage", (data) => {
+    messages = [...messages, data];
+    socket.broadcast.emit("setMessage", messages);
+    console.log(messages);
+    socket.emit("setMessage", messages);
+  });
+});
+
+server.listen(3000, () => console.log("Server is running on port 3000 !"));
--- code/src/pages/Community.jsx
+++ code/src/pages/Community.jsx
@@ -2,11 +2,25 @@ import { faRightLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useScreenSize from "../hooks/useScreenSize";
 import { useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const options = {
+  "force new connection": true,
+  reconnectionAttempts: Infinity,
+  timeout: 10000,
+  transports: ["websocket"],
+  reconnection: true,
+};
 
 function Community() {
   const isMobile = useScreenSize();
   const [message, setMessage] = useState("");
   const textareaRef = useRef(null);
+  const [online, setOnline] = useState(0);
+  const socketRef = useRef(null);
+  // changes everytime due to re-render !!!
+  const id = crypto.randomUUID();
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -15,15 +29,39 @@ function Community() {
     }
   }, [message]);
 
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000", options);
+    socketRef.current.on("setUsers", (users, msg) => {
+      setOnline(users);
+      setMessages(msg);
+    });
+    socketRef.current.on("setMessage", (msg) => {
+      setMessages(msg);
+    });
+  }, []);
+
+  const handleSend = () => {
+    if (message && socketRef.current) {
+      const data = {
+        id: id,
+        message: message,
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString(),
+      };
+      socketRef.current.emit("updateMessage", data);
+      setMessage("");
+    }
+  };
+
   return (
     <div className="p-5 flex flex-col sm:flex-row w-screen h-[95vh] space-x-0 sm:space-x-8 space-y-4 sm:space-y-0">
       {!isMobile && (
-        <div className="w-full sm:w-3/4 h-full rounded-md ring-4 ring-offset-6 ring-[#b48e92] overflow-scroll">
+        <div className="w-full sm:w-3/4 h-full rounded-md ring-4 ring-offset-6 ring-[#b48e92]">
           <div className="min-h-[1.5rem] bg-teal-200/70">
             <h1 className="p-2 text-center">Upcoming Q&A Sessions</h1>
           </div>
           <div className="">
-            {Array.from({ length: 10 }, (_, i) => (
+            {Array.from({ length: 1 }, (_, i) => (
               <div
                 className="w-full min-h-16 p-4 flex items-center justify-center"
                 key={i}
@@ -39,13 +77,26 @@ function Community() {
           </div>
         </div>
       )}
-      <div className="w-full sm:w-1/4 h-full rounded-md ring-4 ring-offset-6 ring-[#b48e92] flex flex-col justify-between">
-        <div className="min-h-[1.5rem] bg-teal-200/70 flex justify-between p-2">
+      <div className="w-full sm:w-1/4 rounded-md ring-4 ring-offset-6 ring-[#b48e92] flex flex-col justify-between">
+        <div className="min-h-[2.5rem] bg-teal-200/70 flex justify-between p-2">
           <h1 className="text-center">Chat with Anonymous Peers</h1>
           <div className="flex space-x-2 items-center justify-center">
             <div className="w-[10px] h-[10px] bg-green-500 rounded-full"></div>
-            <p>0</p>
+            <p>{online}</p>
           </div>
+        </div>
+        <div className="flex p-2 flex-col overflow-y-auto h-full">
+          {messages?.map((message, idx) => (
+            <p
+              key={idx}
+              className={`${message.id === id ? "self-end" : ""} ${console.log(
+                message.id,
+                id
+              )}p-2 m-2 bg-orange-200 rounded-md w-fit`}
+            >
+              {message.message}
+            </p>
+          ))}
         </div>
         <div className="bg-green-500 w-full flex items-center justify-between p-2 space-x-2">
           <textarea
@@ -55,7 +106,7 @@ function Community() {
             onChange={(e) => setMessage(e.target.value)}
             rows={1}
           />
-          <button className="rounded-md self-end">
+          <button className="rounded-md self-end" onClick={handleSend}>
             <FontAwesomeIcon icon={faRightLong} className="text-white" />
           </button>
         </div>
